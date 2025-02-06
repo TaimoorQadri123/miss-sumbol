0d751ed83b42cbe25ec7a7180a46f370cccc7c38
TaimoorQadri123 <dchota374@gmail.com>
2025-02-05 20:49:55 -0800

    task 4

---
+++ cls4tsk.php
@@ -0,0 +1,60 @@
+<!-- question 7  -->
+ <?php 
+//  $color = ['white','green','red','blue','black'];
+//  echo "The memory of that scene for me is like a frame of film foreever frozen at that moment: the $color[2] carpet,the $color[1] lawn,the $color[0] house,the leaden sky.The new president and his first lady .-Richard M.Nixon ";
+ ?>
+
+<!-- $arr = ["Ali","Ahmed","Yaseen","Farooq","Hammad","Khan"];
+for($i=0 ; $i <= count($arr)-1 ; $i++){
+    echo($arr[$i]."<br>");
+} -->
+
+<!-- question 8  -->
+<?php 
+//  echo " 1 2 3 4 5 6 7 8 9 10";
+?>
+<!-- question  9-->
+<?php 
+
+// Outer loop for each row
+// for ($i = 1; $i <= 5; $i++) {
+    // Inner loop to print stars in each row
+    // for ($j = 1; $j <= $i; $j++) {
+        // echo "*";
+    // }
+    // Move to the next line after printing each row
+    // echo "<br>";
+// }
+?>
+
+<!-- question  10-->
+<?php 
+//  $a = 01;
+//  while($a<=100){
+   
+//      echo "$a ,   ";
+//      $a=$a+1;
+//  }
+?>
+
+
+<!-- question  10-->
+<?php
+// function isLowerCase($str) {
+   
+//     if ($str === strtolower($str)) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+
+// $string = "hI";
+// if (isLowerCase($string)) {
+//     echo "The string is all lowercase.";
+// } else {
+//     echo "The string is not all lowercase.";
+// }
+?>
+
